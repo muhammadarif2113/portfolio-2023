3f5edace21d1dd42dac94e70366fcaec429673ea
muhammadarif2113 <arifmuhammad2113@gmail.com>
2023-05-20 17:17:47 -0400

    animations

--- portfolio/script.js
+++ portfolio/script.js
@@ -29,4 +29,18 @@ $(document).ready(function() {
   });
 }); 
 
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if(entry.isIntersecting) {
+//       entry.target.classList.add('show'); 
+//     } else {
+//       entry.target.classList.remove('show'); 
+//     }
+//   })
+// })
+
+// const hiddenElements = document.querySelectorAll('hidden'); 
+// hiddenElements.forEach((el) => observer.observe(el)); 
+
+
 
